0569af95a6574966979cc76c313e591f8e26e4e5
KomalReddyK <82363361+komalreddy3@users.noreply.github.com>
2023-06-15 22:49:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ document.querySelector("img.img2").setAttribute("src", randomImageSource2);
 
 //If player 1 wins
 if (randomNumber1 > randomNumber2) {
-  document.querySelector("h2").innerHTML = "🚩 Play 1 Wins!";
+  document.querySelector("h2").innerHTML = "🚩 Player 1 Wins!";
 }
 else if (randomNumber2 > randomNumber1) {
   document.querySelector("h2").innerHTML = "Player 2 Wins! 🚩";
